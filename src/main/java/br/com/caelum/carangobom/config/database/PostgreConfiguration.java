9aa84c1ff2bf7391858c55d8ad6fec903d003058
Gabriel Loureiro Gomes <gabrielloureirogomes@MBP-de-Gabriel.lan>
2021-07-06 16:08:33 -0300

    fix code smells

--- src/main/java/br/com/caelum/carangobom/config/database/PostgreConfiguration.java
+++ src/main/java/br/com/caelum/carangobom/config/database/PostgreConfiguration.java
@@ -11,14 +11,15 @@ public class PostgreConfiguration {
 
     }
 
-    public static Connection getDatabaseConnection() {
+    public static Connection getDatabaseConnection() throws SQLException {
 
         String databaseUrl = System.getenv().get("DATABASE_URL");
         String databaseUser = System.getenv().get("DATABASE_USER");
         String databasePassword = System.getenv().get("DATABASE_PASSWORD");
         Connection conn = null;
+        Properties props;
 
-        Properties props = new Properties();
+        props = new Properties();
         props.setProperty("user", databaseUser);
         props.setProperty("password", databasePassword);
 
@@ -26,6 +27,9 @@ public class PostgreConfiguration {
             conn = DriverManager.getConnection(databaseUrl, props);
         } catch (SQLException e) {
             e.getCause();
+        } finally {
+            assert conn != null;
+            conn.close();
         }
         return conn;
     }
