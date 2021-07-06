1dd4c79192b0038c8e59579ba08513a8b499d147
Gabriel Loureiro Gomes <gabrielloureirogomes@MBP-de-Gabriel.lan>
2021-07-06 16:12:46 -0300

    fix security hotspot

--- src/main/java/br/com/caelum/carangobom/controller/BrandController.java
+++ src/main/java/br/com/caelum/carangobom/controller/BrandController.java
@@ -3,7 +3,7 @@ package br.com.caelum.carangobom.controller;
 import br.com.caelum.carangobom.domain.Brand;
 import br.com.caelum.carangobom.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public class BrandController {
     @Autowired
     private BrandRepository brandRepository;
 
-    @RequestMapping("/v1/brand")
+    @GetMapping("/v1/brand")
     public List<Brand> findAll() {
         return brandRepository.findAll();
     }
