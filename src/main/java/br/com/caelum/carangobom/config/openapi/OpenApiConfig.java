c317a0d6a3a16aa120c350ac9276fd88311ae1a9
Arthur Diniz <arthurvdinizs@gmail.com>
2021-07-23 17:53:57 -0300

    refactor(pom): add exclusion files to sonar

--- src/main/java/br/com/caelum/carangobom/config/openapi/OpenApiConfig.java
+++ src/main/java/br/com/caelum/carangobom/config/openapi/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package br.com.caelum.carangobom.openapi;
+package br.com.caelum.carangobom.config.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
