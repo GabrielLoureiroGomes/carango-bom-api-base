d8b178306856a255defa014ddb5aa56ef54c97eb
Gabriel Loureiro Gomes <gabrielloureirogomes@MacBook-Pro-de-Gabriel.local>
2021-07-26 13:55:27 -0300

    improve sonar coverage

--- src/test/java/br/com/caelum/carangobom/service/AuthServiceTest.java
+++ src/test/java/br/com/caelum/carangobom/service/AuthServiceTest.java
@@ -1,25 +1,59 @@
 package br.com.caelum.carangobom.service;
 
 import br.com.caelum.carangobom.domain.User;
+import br.com.caelum.carangobom.exception.BrandNotFoundException;
+import br.com.caelum.carangobom.exception.UserNotFoundException;
+import br.com.caelum.carangobom.mocks.UserMocks;
 import br.com.caelum.carangobom.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 class AuthServiceTest {
 
-    @InjectMocks
+    @Mock
     private UserRepository userRepository;
 
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    public void getMocks() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
+    @DisplayName("LOAD USER BY USERNAME")
     void testShouldLoadUserByUsername() {
-        when(userRepository.findByUsername(Mockito.anyString())).thenReturn(Optional.of(new User()));
-        Optional<User> user = assertDoesNotThrow(() -> userRepository.findByUsername(Mockito.anyString()));
+        String username = "gabriel";
+        when(userRepository.findByUsername(any())).thenReturn(Optional.of(UserMocks.getGabriel()));
+
+        User expected = userService.findUserByUsername(username);
+
+        assertEquals(expected, UserMocks.getGabriel());
+        verify(userRepository, times(1)).findByUsername(any());
+    }
+
+    @Test
+    @DisplayName("LOAD USER BY NON EXISTING USERNAME")
+    void testShouldThrowErrorWhenTryLoadUserByUsername() {
+        String username = "gabriel";
+        given(userRepository.findByUsername(username)).willReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.findUserByUsername(username));
+        verify(userRepository, times(1)).findByUsername(any());
     }
 
 }
