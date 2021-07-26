b46fe3c13a92ab1f2d13a574d941497c7e3af71b
Arthur Diniz <arthurvdinizs@gmail.com>
2021-07-26 14:19:32 -0300

    refactor(user): remover getter username and password

--- src/main/java/br/com/caelum/carangobom/domain/User.java
+++ src/main/java/br/com/caelum/carangobom/domain/User.java
@@ -49,14 +49,4 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
 }
